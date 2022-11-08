c5dc0f77ae4a3c066b86ae94a4bcfa3b1aecc643
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2022-11-08 21:33:24 +0700

    update create.jsx
    change text input into text area with specific height

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -1,6 +1,6 @@
 import Header from "../header/Header";
 import 'bootstrap/dist/css/bootstrap.css';
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import nextId from "react-id-generator";
@@ -14,21 +14,21 @@ const Create = () => {
 
     const [news, setNews] = useState({
         id: 0,
-        title:"",
+        title: "",
         writter: "",
-        body:"",
+        body: "",
     })
 
     const onChangeHandler = (event) => {
-        const {name, value} = event.target;
-        setNews({...news, [name]:value})
+        const { name, value } = event.target;
+        setNews({ ...news, [name]: value })
     }
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
         if (news.title.trim() === "" || news.writter.trim() === "" || news.body.trim() === "") return;
 
-        dispatch(addNews({...news, id}));
+        dispatch(addNews({ ...news, id }));
         setNews({
             id: 0,
             title: "",
@@ -39,48 +39,51 @@ const Create = () => {
 
     return (
         <>
-        <Header/>
-        <Container fluid>
-            <Row className="mt-3">
-                <Col>
-                    <Card bg="secondary" text="white">
-                        <Card.Body>
-                        <Form.Group className="mb-3" controlId="formBasicEmail" onSubmit={onSubmitHandler}>
-                            <Form.Label>News Title</Form.Label>
-                            <Form.Control 
-                                type="text"
-                                name="title"
-                                value={news.title}
-                                onChange={onChangeHandler}
-                            />
-                            <Form.Label>News Writter</Form.Label>
-                            <Form.Control 
-                                type="text"
-                                name="writter"
-                                value={news.writter}
-                                onChange={onChangeHandler}
-                            />
-                            <Form.Label>News Body</Form.Label>
-                            <Form.Control 
-                                type="text"
-                                name="body"
-                                value={news.body}
-                                onChange={onChangeHandler}
-                            />
-
-                            <Button 
-                            variant="primary" 
-                            className="mt-3 text-center"
-                            onClick={() => {
-                                navigate("/")
-                            }}
-                            >SUBMIT</Button>
-                        </Form.Group>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+            <Header />
+            <Container fluid>
+                <Row className="mt-3">
+                    <Col>
+                        <Card bg="secondary" text="white">
+                            <Card.Body>
+                                <Form.Group className="mb-3" controlId="formBasicEmail" onSubmit={onSubmitHandler}>
+                                    <Form.Label>News Title</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="title"
+                                        value={news.title}
+                                        onChange={onChangeHandler}
+                                    />
+                                    <Form.Label>News Writter</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="writter"
+                                        value={news.writter}
+                                        onChange={onChangeHandler}
+                                    />
+                                    <Form.Label>News Body</Form.Label>
+                                    <FloatingLabel controlId="floatingTextarea2" label="Description">
+                                        <Form.Control
+                                            as="textarea"
+                                            type="text"
+                                            name="body"
+                                            value={news.body}
+                                            style={{ height: '200px' }}
+                                            onChange={onChangeHandler}
+                                        />
+                                    </FloatingLabel>
+                                    <Button
+                                        variant="primary"
+                                        className="mt-3 text-center"
+                                        onClick={() => {
+                                            navigate("/")
+                                        }}
+                                    >SUBMIT</Button>
+                                </Form.Group>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
